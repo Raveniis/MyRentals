2f05b0b7d2d275add0a6d31d0c9010b566c5ddc2
tyusuri109 <160405543+tyusuri109@users.noreply.github.com>
2024-03-23 08:30:07 +0800

    Update 2024_03_16_133840_create_tenant_applications_table.php

--- database/migrations/2024_03_16_133840_create_tenant_applications_table.php
+++ database/migrations/2024_03_16_133840_create_tenant_applications_table.php
@@ -4,39 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTenantsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('tenant_applications', function (Blueprint $table) {
-            $table->id();
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger('user_id'); 
+            $table->unsignedBigInteger('application_id');
+            $table->string('emergency_contact');
+            $table->timestamps(); 
 
-            $table->unsignedBigInteger('tenant_id')->nullable();
-            $table->foreign('tenant_id')->references('id')->on('users')->onDelete('cascade');
-
-            $table->unsignedBigInteger('rental_id')->nullable();
-            $table->foreign('rental_id')->references('id')->on('house_rentals')->onDelete('cascade');
-
-            $table->integer('occupants_number');
-            $table->date('move_in_date');
-            $table->integer('lease_term');
-            $table->decimal('monthly_income', 11, 2);
-            $table->string('employment_status');
-            $table->string('application_status');
-            $table->string('remarks')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('application_id')->references('id')->on('applications')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('tenant_applications');
+        Schema::dropIfExists('tenants');
     }
-};
+}
