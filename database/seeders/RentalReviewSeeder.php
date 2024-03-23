417ae21333a214fc08ff70227f6b3075c2505cc7
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-03-23 08:37:47 +0800

    Revert "Revert "update""
    This reverts commit a433675058404ce41707317478bdf7723a923d85.

--- database/seeders/RentalReviewSeeder.php
+++ database/seeders/RentalReviewSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\RentalReview;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class RentalReviewSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\RentalReview::factory(10)->create();
+        RentalReview::factory(10)->create();
     }
 }
